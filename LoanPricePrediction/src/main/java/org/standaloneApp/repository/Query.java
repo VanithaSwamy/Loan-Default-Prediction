ec7da152d6021aa4497761688b80ab001473b553
thechaitu <chaitranatekar79@gmail.com>
2024-12-27 10:19:54 +0530

    Loan Type Queries

--- LoanPricePrediction/src/main/java/org/standaloneApp/repository/Query.java
+++ LoanPricePrediction/src/main/java/org/standaloneApp/repository/Query.java
@@ -17,5 +17,11 @@ public class Query {
 	public static String getAdmincredentials="select * from admin where name=? and password=?;";
 	
 	public static String getBorrowerDetail = "Select * from borrower where name = ? AND id_proof=?";
+	
+	//loan type
 	public static String getLoanType = "select * from loan_type;";//to list all loan details
+	public static String setNewLoanType = "insert into loan_type values(0,?);";
+	public static String getLoanTypeId = " select loan_type_id from loan_type where type_name =?;";//to get loan id
+	public static String updateLoanTypeName = "update loan_type set type_name = ? where loan_type_id = ?;";//to update name via id
+	public static String deleteLoanTypeName = "delete from loan_type where loan_type_id = ? ;";
 }
