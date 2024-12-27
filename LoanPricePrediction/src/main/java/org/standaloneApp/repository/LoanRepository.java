9ab1e27b99350df2e2f4510f7a6b4c970981b018
thechaitu <chaitranatekar79@gmail.com>
2024-12-27 10:22:48 +0530

    Loan Type Repo

--- LoanPricePrediction/src/main/java/org/standaloneApp/repository/LoanRepository.java
+++ LoanPricePrediction/src/main/java/org/standaloneApp/repository/LoanRepository.java
@@ -3,4 +3,9 @@ import java.util.*;
 
 public interface LoanRepository {
 	public Optional<Map<Integer,String>> getLoanType();//to get all avail loan types
+	
+	public boolean isAddNewLoanType(String newName);//to insert new loan type
+	public int getLoanId(String oldName);//to get loan id via loan name
+	public boolean updateLoanName(String currName,String newName);//to update loan name
+	public boolean deleteLoanName(String currName);//to delete loan name record
 }
--- LoanPricePrediction/src/main/java/org/standaloneApp/repository/LoanRepositoryImpl.java
+++ LoanPricePrediction/src/main/java/org/standaloneApp/repository/LoanRepositoryImpl.java
@@ -22,5 +22,77 @@ public class LoanRepositoryImpl extends DBState implements LoanRepository {
 			return Optional.empty();//if an error occur 
 		}
 	}
+	
+	//to fetch loan id via name
+	@Override
+	public int getLoanId(String oldName) {
+		try {
+				stmt = conn.prepareStatement(Query.getLoanTypeId);
+				stmt.setString(1, oldName);
+				rs = stmt.executeQuery();
+				
+				if(rs.next()) 
+					return rs.getInt(1); //if id found
+				else 
+					return -1;//if id not found
+				
+		}catch(Exception ex) {
+			System.out.println("Error in GetLoanId: "+ex);
+			return -1;
+		}
+		
+	}
+
+	@Override
+	public boolean updateLoanName(String currName,String newName) {
+		try {
+				int loanId = this.getLoanId(currName);
+				if(loanId !=-1) {
+					stmt = conn.prepareStatement(Query.updateLoanTypeName);
+					stmt.setString(1, newName);
+					stmt.setInt(2, loanId);
+					int val = stmt.executeUpdate();
+					
+					return val > 0 ? true : false;
+				}
+				else
+					return false;
+		}catch(Exception ex) {
+			System.out.println("Error in Update LoanName: "+ex);
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isAddNewLoanType(String newName) {
+		try {
+				stmt = conn.prepareStatement(Query.setNewLoanType);
+				stmt.setString(1, newName);
+				int val = stmt.executeUpdate();
+				return val > 0 ? true : false;
+		}catch(Exception ex) {
+			System.out.println("Error in setNewLoanName: "+ex);
+			return false;
+		}
+	}
+
+	@Override
+	public boolean deleteLoanName(String currName) {
+		try {
+			int loanID = this.getLoanId(currName);
+			if(loanID !=-1) {
+				stmt = conn.prepareStatement(Query.deleteLoanTypeName);
+				stmt.setInt(1, loanID);
+				int val = stmt.executeUpdate();
+				return val > 0 ? true : false;
+			}else
+				return false;
+			
+		}catch(Exception ex) {
+			System.out.println("Error in DeleteLoanName: "+ex);
+			return false;
+		}
+
+	}
 
 }
