1b340106f6efac3af694bbd016402423ea14446c
VanithaSwamy <vanithaswamy196@gmail.com>
2024-12-28 06:41:12 +0530

    Added Functionality

---
+++ LoanPricePrediction/src/main/java/org/standaloneApp/model/CreditModel.java
@@ -0,0 +1,13 @@
+package org.standaloneApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditModel extends IncomeModel {
+	private int cred_id;
+	private int cred_score;
+}
